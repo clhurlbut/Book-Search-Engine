e2c5b9ea2249b1f20926b11cfee8c304da5ee3e3
CassBut <clhurlbut@gmail.com>
2021-03-21 11:57:43 -0700

    added in typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,8 +3,33 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
+  type Auth {
+      token: ID!
+      user: User
+  }
+  type Book {
+      bookId: String
+      authors: [String]
+      description: String
+      title: String
+      image: String
+      link: String 
+  }
+  type User {
+      _id: ID
+      username: String
+      email: String
+      bookCount: Int
+      savedBooks: [Book]
+  }
   type Query {
-    helloWorld: String
+      me: User
+  }
+  type Mutation {
+      login(email: String!, password: String!): Auth
+      addUser(username: String!, email: String!, password: String!): Auth
+      saveBook(input: bookInput): User
+      removeBook(bookId: String!): User
   }
 `;
 
